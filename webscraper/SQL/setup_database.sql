8afdc9d34f19ec884bb4a4475dc5286a0dadd27b
Domenic Sawatzki <domenic.sawatzki@gmail.com>
2023-09-20 10:00:54 +0200

    Added gitignore and created database

---
+++ webscraper/SQL/setup_database.sql
@@ -0,0 +1,25 @@
+-- Active: 1691744613924@@127.0.0.1@3306
+USE final_project;
+CREATE TABLE IF NOT EXISTS linked_in_scrap
+(
+id INTEGER PRIMARY KEY,
+title varchar(50),
+company varchar(50),
+posting_date date,
+job_description JSON,
+seniority_level varchar(50),
+job_function varchar(50),
+industries varchar(50),
+scraping_date date,
+url varchar(500),
+keyword varchar(50)
+)
+;
+
+
+CREATE TABLE IF NOT EXISTS soup_archiv
+(
+id INTEGER PRIMARY KEY,
+little_soup json
+);
+
